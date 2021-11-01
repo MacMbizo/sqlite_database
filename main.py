d97933d9bf50166930fbf9e55b5e36b00948c537
Mac Mbizo <macmbizo@gmail.com>
2021-11-01 22:11:34 +0200

    added database people.db

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ try:
 except Exception as e:
     pass
 
+def user_is_unique(name):
+    rows = cursor.execute("SELECT name, age, skills FROM pepole").fetchall()
+
+    for user in rows:
+        if user[0] == name:
+            return False
+    return True
+
+def insert_db():
+    name = input("Name >>")
+
+    if user_is_unique(str(name)):
+        age = input("Age >>")
+        skills = input("Skills >>")
+
+        if name !="" and age != "" and skills != "":
+            cursor.execute(f"INSER INTO EOLE")
